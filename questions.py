1d786dfc917a906a05e5822a8ba2d79bb1282c93
unknown <edimariobabau@gmail.com>
2021-10-21 02:39:54 -0300

    inital commit

---
+++ questions.py
@@ -0,0 +1,32 @@
+quiz_questions = [
+    {
+        'question': 'Quando o COVID-19 surgiu?',
+        'alternatives': ['2019','2020','2018'],
+        'right_answer': 0
+    },
+    {
+        'question': 'Qual destas vacinas NÃO é do COVID-19?',
+        'alternatives': ['Pfizer','Astrazeneca','Pertacel'],
+        'right_answer': 2
+    },
+    {
+        'question': 'Qual coisa NÃO é eficaz para prevenir o COVID-19?',
+        'alternatives': ['Hidroxicloroquina','Máscara','Quarentena'],
+        'right_answer': 0
+    },
+    {
+        'question': 'Tomei a vacina da gripe e estou protegido.',
+        'alternatives': ['Fato', 'Fake'],
+        'right_answer': 1
+    },
+     {
+        'question': 'O vírus NÃO sobrevive por muito tempo nas superfícies.',
+        'alternatives': ['Fato', 'Fake'],
+        'right_answer': 1
+    },
+     {
+        'question': 'Quem já teve COVID-19 também precisa tomar vacina.',
+        'alternatives': ['Fato', 'Fake'],
+        'right_answer': 0
+    },
+]
